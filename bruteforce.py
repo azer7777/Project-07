9fcec46b3f6c9db1524d48e7705eb16780f00be2
azer7777 <azertyuio123a77@gmail.com>
2023-07-21 09:01:49 +0200

    donne brute force

--- bruteforce.py
+++ bruteforce.py
@@ -6,31 +6,44 @@ MAX_SPEND = 500
 
 
 def get_data(path):
-    share_list = []
+    all_shares = []
     with open(path, 'r') as file:
         csvreader = csv.reader(file)
         next(csvreader)
         for row in csvreader:
-            share_list.append(row)
-    return share_list
+            all_shares.append((row[0], float(row[1]), float(row[2])))
+    return all_shares
 
-def invest_limit(path):
-    price = (get_data(path))[1]
-    benefit = (get_data(path))[2]
  
-
-
 def get_best_combination(path):
-    shares_list = get_data(path)
-    profit = 0
-    best_combination = []
-    cost = []
-    for i in range(3):
-        combination = list(combinations(shares_list, i))
-        for e in combination[1]:
-            cost.append(e)
-       
-    print(cost)
+    all_shares = get_data(path)
+    final_profit = 0
+    all_combination = []    
+    best_combination = ""
+    for i in range(len(all_shares)):
+        all_combination += list(combinations(all_shares, i + 1))
+        for one_combination in all_combination:
+            share_name_list = []
+            share_price_list = []
+            share_profit_list = []            
+            for i in one_combination:
+                share_name = i[0]
+                share_price = i[1]  
+                share_gain = i[2]
+                profit_by_share = (share_price * share_gain) / 100            
+                share_price_list.append(share_price)
+                share_profit_list.append(profit_by_share)
+                share_name_list.append(share_name)                               
+            spend = sum(share_price_list)
+            profit = sum(share_profit_list)
+            if spend <= MAX_SPEND and profit >= final_profit:
+                investement = spend
+                final_profit = profit
+                best_combination = share_name_list
+    print("Best combination : ", best_combination)
+    print("Investement : ", round(investement, 2), "$")
+    print("Profit : ", round(final_profit, 2), "$")
+    return
 
     
-get_best_combination("data/dataset0_Python+P7.csv")   
+get_best_combination("data/dataset4_Python+P7.csv")   
